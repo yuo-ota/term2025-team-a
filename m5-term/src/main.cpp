c22ffb1f9a30016515cf8b897b254e1aa6c4b7bf
Tooyaes <23fi005@ms.dendai.ac.jp>
2025-12-10 17:58:32 +0900

    feat/mockデータをjson形式で送信する機能を追加

--- m5-term/src/main.cpp
+++ m5-term/src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <M5Stack.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// WiFi設定
+const char *ssid = "yourssid";
+const char *password = "yourPASSWORD";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// mockData
+float temp = 25.5;
+float humidity = 60.0;
+String deviceId = "Device123";
+
+const char *serverUrl = "http://[IPAdress]:[PortNumber]/api/m5data";
+
+void setup()
+{
+    M5.begin();
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        M5.Lcd.printf("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+    M5.Lcd.printf("Connected to WiFi\n");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    StaticJsonDocument<200> doc;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    doc["device_id"] = deviceId;
+    doc["temperature"] = temp;
+    doc["humidity"] = humidity;
+
+    String jsonpayload;
+    serializeJson(doc, jsonpayload);
+
+    HTTPClient http;
+    http.begin(serverUrl);
+
+    http.addHeader("Content-Type", "application/json");
+
+    int httpResponseCode = http.POST(jsonpayload);
+
+    if (httpResponseCode > 0)
+    {
+        Serial.print("HTTP Response code: ");
+        M5.Lcd.printf("HTTP Response code: %d\n", httpResponseCode);
+        Serial.println(httpResponseCode);
+        String response = http.getString();
+        Serial.println(response);
+        M5.Lcd.println(response);
+    }
+    else
+    {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+        M5.Lcd.printf("Error code: %d\n", httpResponseCode);
+    }
+    http.end();
+    delay(10000); // 10秒待機
 }
